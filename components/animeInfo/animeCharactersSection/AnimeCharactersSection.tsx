293ef4bee0587f155b7fa3d4d8e00addeabf7851
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2024-10-16 20:09:12 +0200

    build error

--- components/animeInfo/animeCharactersSection/AnimeCharactersSection.tsx
+++ components/animeInfo/animeCharactersSection/AnimeCharactersSection.tsx
@@ -1,28 +1,28 @@
-import { ICharacters } from "../../../constants/sectionInterfaces/ICharacters";
+import { ICharacters } from "../../../constants/sectionInterfaces/charactersInterface";
 import CharacterCard from "../animeInfoSection/characters/characterCard";
 export default function AnimeCharactersSection({
-	characters,
+  characters,
 }: {
-	characters: ICharacters[];
+  characters: ICharacters[];
 }) {
-	return (
-		<section>
-			{characters && characters.length >= 1 ? (
-				<ul>
-					{characters.map((character, i) => (
-						<CharacterCard
-							key={i}
-							characterInfo={character}
-							index={i}
-							isPage={true}
-						/>
-					))}
-				</ul>
-			) : (
-				<p className="mt-4 text-sm">
-					No characters or voice actors have been added to this title
-				</p>
-			)}
-		</section>
-	);
+  return (
+    <section>
+      {characters && characters.length >= 1 ? (
+        <ul>
+          {characters.map((character, i) => (
+            <CharacterCard
+              key={i}
+              characterInfo={character}
+              index={i}
+              isPage={true}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="mt-4 text-sm">
+          No characters or voice actors have been added to this title
+        </p>
+      )}
+    </section>
+  );
 }
