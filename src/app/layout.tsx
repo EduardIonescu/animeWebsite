4955aeaafaaf8112d88ceaf45cc64817ecaba4aa
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-03-04 21:29:56 +0100

    change font to poppins + local fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar/navbar";
 import BackToTopButton from "@/components/other/backToTopButton";
 import { ThemeProvider } from "@/components/other/themeProvider";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -9,13 +10,38 @@ export const metadata: Metadata = {
   description: "Anime database website",
 };
 
+const poppins = localFont({
+  src: [
+    {
+      path: "./fonts/Poppins-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./fonts/Poppins-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Poppins-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Poppins-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "./fonts/Poppins-Bold.ttf",
+      weight: "700",
+    },
+  ],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={poppins.className}>
       <head>
         <link rel="icon" href="/icon.png" sizes="any" />
       </head>
