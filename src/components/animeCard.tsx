238c4ed055e7a7115ced4a5f84b4bacbfba6a6f5
EduardIonescu <37626215+EduardIonescu@users.noreply.github.com>
2025-03-05 17:04:22 +0100

    cards

--- src/components/animeCard.tsx
+++ src/components/animeCard.tsx
@@ -6,7 +6,13 @@ import { IsAnimeData } from "@/types/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function AnimeCard({ animeData }: { animeData: IsAnimeData }) {
+export default function AnimeCard({
+  animeData,
+  index,
+}: {
+  animeData: IsAnimeData;
+  index: number;
+}) {
   const imageUrl = animeData.images.webp.image_url;
   const title = animeData.title_english;
   const titleJapanese = animeData.title;
@@ -14,65 +20,106 @@ export default function AnimeCard({ animeData }: { animeData: IsAnimeData }) {
   const year = animeData.aired.prop.from.year;
   const id = animeData.mal_id;
   return (
-    <Link href={`/anime/${id}`}>
-      <article
-        /* card class for linear-gradient background transition */
-        className="card w-[100%] md:w-[21rem] lg:w-[28.5rem] xl:w-[380px]
-				 border-[1px] flex items-center 
-				transition max-w-[calc(100vw-32px)] sm:max-w-[512px]
-			duration-300 gap-4 shadow-md hover:-translate-x-[1px] relative
-			hover:-translate-y-[2px] cursor-pointer hover:shadow-xl group rounded 
-			overflow-hidden dark:bg-darkBlueDark dark:border-darkBlue 
+    <>
+      <Link href={`/anime/${id}`} className="lg:group">
+        <article
+          /* card class for linear-gradient background transition */
+          className="card lg:hidden w-[100%] md:w-[21rem] border-[1px] 
+        flex items-center transition max-w-[calc(100vw-32px)] sm:max-w-[512px]
+			  duration-300 gap-4 shadow-md hover:-translate-x-[1px] relative
+			  hover:-translate-y-[2px] cursor-pointer hover:shadow-xl group rounded 
+			  overflow-hidden dark:bg-darkBlueDark dark:border-darkBlue 
 			dark:hover:shadow-black/30 dark:hover:shadow-lg "
-      >
-        <div className="relative h-[120px] w-[85px] shrink-0">
-          <Image
-            src={imageUrl}
-            fill
-            sizes="100%"
-            alt=""
-            aria-hidden="true"
-            className="object-cover "
-          />
-        </div>
-        {/* Maybe switch whitespace-nowrap to line-clamp */}
-        <div className="whitespace-nowrap overflow-hidden pb-1">
-          <div
-            className="absolute top-0 text-[11px] bg-darkBlue/75
+        >
+          <div className="relative h-[120px] w-[85px] shrink-0">
+            <Image
+              src={imageUrl}
+              fill
+              sizes="100%"
+              alt=""
+              aria-hidden="true"
+              className="object-cover "
+            />
+          </div>
+          {/* Maybe switch whitespace-nowrap to line-clamp */}
+          <div className="whitespace-nowrap overflow-hidden pb-1">
+            <div
+              className="absolute top-0 text-[11px] bg-darkBlue/75
 				text-shadowLightBlue flex items-center gap-1 py-[2px] px-1 rounded-b-md
 				"
-          >
-            <div className="w-[11px] h-[11px] relative shrink-0 mb-[1px]">
-              <Image
-                src={starIcon}
-                fill
-                sizes="100%"
-                alt=""
-                aria-hidden="true"
-              />
+            >
+              <div className="w-[11px] h-[11px] relative shrink-0 mb-[1px]">
+                <Image
+                  src={starIcon}
+                  fill
+                  sizes="100%"
+                  alt=""
+                  aria-hidden="true"
+                />
+              </div>
+              {score}
             </div>
-            {score}
-          </div>
-          <div
-            className="absolute top-0 right-0 text-[11px] bg-lighterBlue 
+            <div
+              className="absolute top-0 right-0 text-[11px] bg-lighterBlue 
 				text-shadowLightBlue flex items-center gap-1 py-[2px] px-1 rounded-bl-md"
-          >
-            <div className="h-[10px] w-[10px] relative">
-              <Image
-                src={calendarIcon}
-                fill
-                sizes="100%"
-                alt=""
-                aria-hidden="true"
-                className="h-auto"
-              />
-            </div>{" "}
-            {year}
+            >
+              <div className="h-[10px] w-[10px] relative">
+                <Image
+                  src={calendarIcon}
+                  fill
+                  sizes="100%"
+                  alt=""
+                  aria-hidden="true"
+                  className="h-auto"
+                />
+              </div>{" "}
+              {year}
+            </div>
+            <h2 className="text-base font-medium">{title ?? titleJapanese}</h2>
+            <p className="text-sm opacity-60">{titleJapanese}</p>
           </div>
-          <h2 className="text-base font-medium">{title}</h2>
-          <p className="text-sm opacity-60">{titleJapanese}</p>
-        </div>
-      </article>
-    </Link>
+        </article>
+
+        <article
+          className="hidden lg:block w-[280px] relative group transition-all 
+      duration-200 ease"
+        >
+          <div
+            className="absolute left-2 -bottom-1 w-64 text-base font-medium 
+        group-hover:text-lighterBlue dark:group-hover:text-darkYellow 
+        transition-colors duration-200"
+          >
+            <span
+              className="text-lighterBlue dark:text-darkYellow text-xl 
+            font-semibold"
+            >
+              {(index + 1).toString().padStart(2, "0")}
+            </span>
+            <h2
+              className="inline-block -rotate-90 absolute bottom-28 text-base  
+            w-52 -left-24 text-ellipsis text-nowrap overflow-hidden whitespace-nowrap 
+            pl-3 pt-1.5"
+            >
+              {title ?? titleJapanese}
+            </h2>
+          </div>
+
+          <div
+            className="relative w-48 h-72 shrink-0 ml-10 block shadow-black 
+          shadow-md group-hover:shadow-lg group-hover:shadow-black transition-all 
+          duration-300 ease-out group-hover:scale-[101%]"
+          >
+            <Image
+              src={imageUrl}
+              fill
+              sizes="100%"
+              alt=""
+              aria-hidden="true"
+              className="object-cover"
+            />
+          </div>
+        </article>
+      </Link>
+    </>
   );
 }
--- src/components/topAnimeSection.tsx
+++ src/components/topAnimeSection.tsx
@@ -17,11 +17,11 @@ export default function TopAnimeSection({
     return (
       <section
         className="flex flex-wrap flex-col md:flex-row md:gap-4
-      xl:gap-x-[calc((100%-(380px*3))/2)] gap-y-5 pb-6"
+      xl:gap-x-[calc((100%-(380px*3))/2)] gap-y-5 pb-6 lg:justify-between lg:gap-y-7"
       >
         {(!showTrending ? popularAnimeData : trendingAnimeData).map(
-          (animeData, i) => (
-            <AnimeCard key={i} animeData={animeData} />
+          (animeData, index) => (
+            <AnimeCard key={index} animeData={animeData} index={index} />
           )
         )}
       </section>
